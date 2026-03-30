5a93549673036d3ec0324f758c5c982b18786951
Hritik Sinha <sinhahritik72@gmail.com>
2026-03-30 18:26:05 +0530

    learning

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//age vote or not
+const age = prompt("Enter your age") 
+(age >=18) ?  alert("You can vote") : alert("You can't vote")
+
+
+
+// positive & negative  number
+
+const num = prompt("Enter a number")
+if(num > 0){
+    alert("positive number")
+}else{
+    alert("negative number")
+}
+
+
+//grading 0-33 fail & 33-60 pass
+
+const marks = prompt("Enter your marks")
+  if(marks >= 0 && marks <=33){
+    alert("FAIL")
+  }else if(marks >=34 && marks<=66){
+    alert("PASS")
+  }
+// print number 1-30 by for loop
+
+for(let i=1; i<=30; i++){
+    console.log(i);
+}
+
+
+ 
