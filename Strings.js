8e5e967b3aba673392a8fcac13b42fafc82dd7c3
Hritik Sinha <sinhahritik72@gmail.com>
2026-03-31 18:26:51 +0530

    learning3

--- Strings.js
+++ Strings.js
@@ -6,3 +6,37 @@ function Deeksha(){
     
  
 }
+let str = "Apple Mango Banana"
+let arr = str.split(' ')
+console.log(arr);
+
+//Template Literals (` `)
+
+function Shiv() {
+    //1st use    Supports Single('') and double("") quotes
+    //   alert(`Hello i am "Sid" from 'Bhopal' mp.nagar`)
+
+
+    //2nd use Multi line String
+    // let str = `Hello I am 
+    //  Hritik`
+    // console.log(str);
+
+
+    // 3rd use Supports HTML Tags
+   // document.write(`Hello I am <b> Hritik </b> From Bhopal`);
+    
+  // 4th use Support dynamic Varibale
+//   let myname = "Hritik"
+//   let myage  = 23
+//   console.log(` hello my name is ${myname} and ${myage} is `);
+  
+    
+}
+
+function userPromt(){
+
+    const myname = prompt(`what is your name`)
+    const myage = prompt(`What is your age`)
+    document.write(` My name is <b> ${myname} </b> and my age is <b> ${myage} </b> `)
+}
